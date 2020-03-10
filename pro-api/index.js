50ecae1cf3fd31c2b54da454c5217ad89ad3627a
chenshuai2144 <qixian.cs@outlook.com>
2020-03-10 11:26:55 +0800

    init

---
+++ pro-api/index.js
@@ -0,0 +1,24 @@
+const createHandler = require('azure-function-express').createHandler;
+const express = require('express');
+const matchMock = require('./mock/matchMock');
+
+const app = express();
+
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild',
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+
+app.use(matchMock);
+
+export default createHandler(app);
